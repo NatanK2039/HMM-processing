6e3de94fc4273c5f6ee5aac8570fc9e50d5c03a6
Natan Kolodziej <natankolodziej@yahoo.com>
2025-05-05 17:38:38 +0100

    Continued work on file management functionality. Logic simplified nd made less error prone

--- HMM Pre-processing.py
+++ HMM Pre-processing.py
@@ -1,25 +1,54 @@
+import os.path
+
 def main():
-   getFiles()
+    print("This script will automatically perform the processing of a pcap file to encode it in a form suitable for multivariate HMM.")
+    getFiles()
 
 def getFiles():
-    endloop = False;
-    print("This script will automatically perform the processing of a pcap file to encode it in a form suitable for multivariate HMM.")
+    getInputFiles()
 
+def getInputFiles():
+    endloop = False;
 
     while (endloop == False):
-        inputfile = getSingleFile()
-        print("File succesfully added")
-        endloopinput = input("Enter another file path to add another file, or enter Finish to finish adding files.")
+        endloopinput = getUserInput("Enter a file path to add a file, or enter Finish to finish adding files.")
         if endloopinput == "Finish":
             endloop = True
         else:
-            getSingleFile()
+            FileObject = getSingleFile(endloopinput)
+            print("File succesfully added")
 
-def getSingleFile():
-    filepath = input("Please provide the absolute path to the file.")
 
-def checkfileexists():
-    print("stuff")
+
+def getSingleFile(filepath):
+    if filepath == "false":
+        filepath = getUserInput("Enter absolute filepath")
+        checkfileexists(filepath)
+        
+    else: 
+        checkfileexists(filepath)
+
+
+
+
+
+
+def checkfileexists(filepath):
+    if os.path.isfile(filepath):
+        if filepath.lower().endswith(('.pcap', '.pcapng')):
+            print("file exists")
+    else:
+        print("File does not exist, try again.")
+        getInputFiles()
+
+def getUserInput(message):
+    userResponse = input(message)
+    return userResponse
+
+
+
+
+
 
 def createfile():
     print("stuff")

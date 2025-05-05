9038a87ab166a2d168e8feb115c6a0caf0702f23
Natan Kolodziej <natankolodziej@yahoo.com>
2025-05-05 19:04:12 +0100

    File handling almost completed. Next step add file creation functionality and writing to files

--- HMM Pre-processing.py
+++ HMM Pre-processing.py
@@ -1,51 +1,74 @@
 import os.path
+from scapy.all import rdpcap
 
 def main():
     print("This script will automatically perform the processing of a pcap file to encode it in a form suitable for multivariate HMM.")
     getFiles()
 
 def getFiles():
-    getInputFiles()
+    getFiles()
 
-def getInputFiles():
+def getFiles():
     endloop = False;
+    inputAndOutputFiles = {}
 
     while (endloop == False):
         endloopinput = getUserInput("Enter a file path to add a file, or enter Finish to finish adding files.")
         if endloopinput == "Finish":
             endloop = True
         else:
-            FileObject = getSingleFile(endloopinput)
-            print("File succesfully added")
+            inputfile = getInputFile(endloopinput)
+            print("File saved")
+            outputfile = getOutputFile()
 
 
 
-def getSingleFile(filepath):
-    if filepath == "false":
+def getInputFile(filepath):
+    if filepath == "none":
         filepath = getUserInput("Enter absolute filepath")
         checkfileexists(filepath)
-        
+        file = loadFile(filepath)
     else: 
-        checkfileexists(filepath)
+        checkfileexists(filepath, "pcap")
+        file = loadFile(filepath)
+    return file
 
+def getOutputFile():
+    filepath = getUserInput("Where to save encoded data? Provide absolute path to .txt file (The file will be created if it does not yet exist)")
+    checkfileexists(filepath, "txt")
 
-
-
-
-
-def checkfileexists(filepath):
-    if os.path.isfile(filepath):
-        if filepath.lower().endswith(('.pcap', '.pcapng')):
-            print("file exists")
+def checkfileexists(filepath, type):
+    if type == "pcap":
+        if os.path.isfile(filepath):
+            if filepath.lower().endswith(('.pcap', '.pcapng')):
+                print("file exists")
+        else:
+            print("File does not exist, try again.")
+            getFiles()
     else:
-        print("File does not exist, try again.")
-        getInputFiles()
+        if os.path.isfile(filepath):
+            print("File found")
+        else:
+            createfileselection = getUserInput("File does not exist. Create file? Enter yes or no.", "yes", "no")
+            if createfileselection == "yes":
+                createfile()
+            else:
+                getOutputFile()            
 
-def getUserInput(message):
+def loadFile(filepath):
+    file = open(filepath)
+    return file
+
+
+def getUserInput(message, *args):
     userResponse = input(message)
+    if args:
+        if userResponse in args:
+            return userResponse
+        else:
+            return getUserInput("\n\nInvalid input. Answer with: " + ", ".join(args) + "\n", *args)
     return userResponse
-
-
+ 
 
 
 
@@ -56,3 +79,5 @@ def createfile():
 
 
 main()
+#E:\ethical hacking examin\python assesme t\Mal.pcapng
+#
